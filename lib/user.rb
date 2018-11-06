a1946cb10d76111f5947af682672346c2f9e0881
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-06 15:35:42 +0000

    passwords encrypted

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User
 
   attr_reader :id, :name, :username, :email, :password
@@ -11,7 +13,8 @@ class User
   end
 
   def self.register(name:, username:, email:, password:)
-    result = DatabaseConnection.query("INSERT INTO users (name, username, email, password) VALUES ('#{name}', '#{username}', '#{email}', '#{password}')RETURNING id, name, username, email, password;")
+    encrypted_password = BCrypt::Password.create(password)
+    result = DatabaseConnection.query("INSERT INTO users (name, username, email, password) VALUES ('#{name}', '#{username}', '#{email}', '#{encrypted_password}')RETURNING id, name, username, email, password;")
     result.map { |user| User.new(id: user['id'], name: user['name'], username: user['username'], email: user['email'], password: user['password'])  } [0]
   end
 
@@ -19,7 +22,7 @@ class User
     query = "SELECT * FROM users WHERE username = '#{username}'"
     result = DatabaseConnection.query(query)
     return nil unless result.any?
-    return nil unless result[0]['password'] == password
+    return nil unless BCrypt::Password.new(result[0]['password']) == password
     User.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
   end
 
