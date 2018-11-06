692deb197a6e60753e85133d8f946e9fb001b1ca
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-11-06 11:19:00 +0000

    Added property spec and class

--- lib/property.rb
+++ lib/property.rb
@@ -1,6 +1,13 @@
-function Property() {
-  this.available = true
-};
-Property.prototype.isitAvailable = function(){
-  return this.available
-};
+require_relative '../database_connection_setup'
+
+class Property
+
+  attr_reader :name, :description, :ppn
+
+  def initialize(name: 'default name', description: 'default description', ppn: 10)
+    @name = name
+    @description = description
+    @ppn = ppn
+  end
+
+end
--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -1,7 +1,20 @@
+require 'property'
 
-describe('Property', function(){
+describe Property do
 
-  it('Is available to rent', function(){
-    expect(property.isitAvailable()).toEqual(true);
-  });
-});
+  it 'knows its name' do
+    property = Property.new(name: 'Cottage')
+    expect(property.name).to eq('Cottage')
+  end
+
+  it 'knows its description' do
+    property = Property.new(description: 'A lovely place')
+    expect(property.description).to eq('A lovely place')
+  end
+
+  it 'knows its price per night' do
+    property = Property.new(ppn: 15)
+    expect(property.ppn).to eq(15)
+  end
+
+end
