3670d768209ffbc5b54e83c7f4eeae22d6f4bc82
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-07 16:10:31 +0000

    corrected database issue with primary key

--- spec/features/add_property_feature_spec.rb
+++ spec/features/add_property_feature_spec.rb
@@ -21,4 +21,35 @@ feature 'User can add a property' do
     expect(page).to have_content '£10 per night'
 
   end
+
+  it'adds a second property' do
+    user = User.register(name: 'Lucas', username: 'sacullezzar', email: 'lucas.razzell@gmail.com', password: 'pass123')
+
+    visit ('/')
+    click_button 'login'
+
+    fill_in :username, with: 'sacullezzar'
+    fill_in :password, with: 'pass123'
+
+    click_button 'submit'
+    click_button 'Add property'
+
+    fill_in :name, with: 'Cottage'
+    fill_in :description, with: 'Test description'
+    fill_in :ppn, with: '10'
+
+    click_button 'Submit'
+
+    click_button 'Add property'
+
+    fill_in :name, with: 'House'
+    fill_in :description, with: 'Test house'
+    fill_in :ppn, with: '15'
+
+    click_button 'Submit'
+
+    expect(page).to have_content 'Cottage'
+    expect(page).to have_content '£10 per night'
+
+  end
 end
