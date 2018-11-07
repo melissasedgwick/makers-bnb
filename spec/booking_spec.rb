007fd630e0fae196c3234491758f89f4efaec988
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-11-07 11:51:32 +0000

    Added request_booking method to booking class

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -3,30 +3,52 @@ require 'property'
 
 describe Booking do
 
+  before :each do
+    @property = Property.create(name: 'Fancy Place', description: 'A fancy place', ppn: 20)
+    @user = User.register(name: 'Test', username: 'tester', email: 'test@test.com', password: 'testpass')
+  end
+
   describe '#submit_availability' do
     it 'adds availability for property to database' do
-      property = Property.create(name: 'Fancy Place', description: 'A fancy place', ppn: 20)
-      booking = Booking.submit_availability(date: "2018/10/15", property_id: property.id)
+      booking = Booking.submit_availability(date: "2018/10/15", property_id: @property.id)
 
       expect(booking.date).to eq("2018-10-15")
-      expect(booking.property_id).to eq(property.id)
+      expect(booking.property_id).to eq(@property.id)
     end
   end
 
   describe '#check_availability' do
     it 'returns dates a property is available' do
-      property = Property.create(name: 'Fancy Place', description: 'A fancy place', ppn: 20)
-      Booking.submit_availability(date: "2018/10/15", property_id: property.id)
-      Booking.submit_availability(date: "2018/10/17", property_id: property.id)
-      Booking.submit_availability(date: "2018/11/18", property_id: property.id)
+      Booking.submit_availability(date: "2018/10/15", property_id: @property.id)
+      Booking.submit_availability(date: "2018/10/17", property_id: @property.id)
+      Booking.submit_availability(date: "2018/11/18", property_id: @property.id)
 
-      expect(Booking.check_availability(property_id: property.id)).to eq(["2018-10-15", "2018-10-17", "2018-11-18"])
+      expect(Booking.check_availability(property_id: @property.id)).to eq(["2018-10-15", "2018-10-17", "2018-11-18"])
     end
 
     it 'returns nil if no dates' do
-      property = Property.create(name: 'Fancy Place', description: 'A fancy place', ppn: 20)
+      expect(Booking.check_availability(property_id: @property.id)).to eq nil
+    end
+  end
 
-      expect(Booking.check_availability(property_id: property.id)).to eq nil
+  describe '#request_booking' do
+    it 'raises error if requesting unavailable date for property' do
+      Booking.submit_availability(date: "2018/10/15", property_id: @property.id)
+      expect{Booking.request_booking(date: "2018/09/01", property_id: @property.id, renter_id: @user.id)}.to raise_error("Date unavailable")
+    end
+
+    it 'raises error if no dates available' do
+      expect{Booking.request_booking(date: "2018/09/01", property_id: @property.id, renter_id: @user.id)}.to raise_error("Date unavailable")
+    end
+
+    it 'adds request to bookings table if date and property is available' do
+      Booking.submit_availability(date: "2018/10/15", property_id: @property.id)
+
+      booking = Booking.request_booking(date: "2018/10/15", property_id: @property.id, renter_id: @user.id)
+      expect(booking.date).to eq "2018-10-15"
+      expect(booking.property_id).to eq @property.id
+      expect(booking.renter_id).to eq @user.id
+      expect(booking.approved).to eq false
     end
   end
 
