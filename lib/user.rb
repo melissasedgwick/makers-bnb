48ab967177dc65f0ba1d8d35d1b88968627646a0
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-08 10:29:33 +0000

    checks for unique user details

--- lib/user.rb
+++ lib/user.rb
@@ -13,6 +13,8 @@ class User
   end
 
   def self.register(name:, username:, email:, password:)
+    raise('Email already in use!') if User.email_exists?(email)
+    raise('Username already in use!') if User.username_exists?(username)
     encrypted_password = BCrypt::Password.create(password)
     result = DatabaseConnection.query("INSERT INTO users (name, username, email, password) VALUES ('#{name}', '#{username}', '#{email}', '#{encrypted_password}')RETURNING id, name, username, email, password;")
     result.map { |user| User.new(id: user['id'], name: user['name'], username: user['username'], email: user['email'], password: user['password'])  } [0]
@@ -26,4 +28,13 @@ class User
     User.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
   end
 
+  def self.email_exists?(email)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}';")
+    result.any? ? true : false
+  end
+
+  def self.username_exists?(username)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE username = '#{username}';")
+    result.any? ? true : false
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -13,4 +13,22 @@ describe User do
     authenticated_user = User.auth('tester', 'testpass')
     expect(user.id).to eq(authenticated_user.id)
   end
+
+  it 'raises error if email in use' do
+    user = User.register(name: 'Test', username: 'tester', email: 'test@test.com', password: 'testpass')
+    expect{User.register(
+      name: 'Test',
+      username: 'test',
+      email: 'test@test.com',
+      password: 'testpass')}.to raise_error('Email already in use!')
+  end
+
+  it 'raises error if username in use' do
+    user = User.register(name: 'Test', username: 'tester', email: 'test@test.com', password: 'testpass')
+    expect{User.register(
+      name: 'Test',
+      username: 'tester',
+      email: 'test@test.co.uk',
+      password: 'testpass')}.to raise_error('Username already in use!')
+  end
 end
