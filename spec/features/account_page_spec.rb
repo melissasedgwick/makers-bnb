136e1101f80be7d6775a7b87d10f02f0d1b0fce8
Aimée Craig <aimeecraig1@gmail.com>
2018-11-08 14:06:08 +0000

    fixed bug with approval status

--- spec/features/account_page_spec.rb
+++ spec/features/account_page_spec.rb
@@ -70,12 +70,33 @@ feature 'Account page' do
     expect(page).to have_content "Approved"
   end
 
+  it 'shows a pending booking' do
+    letter = User.register(name: "Name", username: "username", email: "test@test.com", password: "password")
+    user = User.register(name: 'Lucas', username: 'sacullezzar', email: 'lucas.razzell@gmail.com', password: 'pass123')
+    property = Property.create(name: "House", description: "Sweet pad!", ppn: 10, letter_id: user.id)
+    Booking.submit_availability(date: "2018/06/06", property_id: property.id, letter_id: letter.id)
+    request = Booking.request_booking(date: "2018/06/06", property_id: property.id, renter_id: user.id)
+
+    visit ('/')
+    click_button 'login'
+    fill_in :username, with: 'sacullezzar'
+    fill_in :password, with: 'pass123'
+    click_button 'submit'
+    click_button 'account_page'
+
+    expect(page).to have_content request.date
+    expect(page).to have_content "Pending"
+  end
+
   it 'lists approved bookings for the logged in user' do
     user = User.register(name: 'Lucas', username: 'sacullezzar', email: 'lucas.razzell@gmail.com', password: 'pass123')
+    user2 = User.register(name: 'Aimee', username: 'ac', email: 'aimee@gmail.com', password: 'pass')
     letter = User.register(name: 'Lucy', username: 'famalam', email: 'penis@flytrap.com', password: 'peniarecool')
     property = Property.create(name: 'Cottage 1', description: 'A lovely place', ppn: 15, letter_id: letter.id)
 
     Booking.submit_availability(date: "2018/06/06", property_id: property.id, letter_id: letter.id)
+    Booking.submit_availability(date: "2018/06/07", property_id: property.id, letter_id: letter.id)
+    Booking.request_booking(date: "2018/06/07", property_id: property.id, renter_id: user2.id)
     request = Booking.request_booking(date: "2018/06/06", property_id: property.id, renter_id: user.id)
     Booking.confirm_booking(id: request.id)
 
@@ -93,5 +114,6 @@ feature 'Account page' do
     expect(page).to have_content('2018-06-06')
     expect(page).to have_content('Cottage 1')
     expect(page).to have_content('15')
+    expect(page).not_to have_content('2018-06-07')
   end
 end
