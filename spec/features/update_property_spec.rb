3f02b8be70eddd7c67d63fe6dfb3ab449d9f5a8d
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-08 15:34:01 +0000

    submit availability implemented

--- spec/features/update_property_spec.rb
+++ spec/features/update_property_spec.rb
@@ -25,4 +25,32 @@ feature 'User can update a property' do
     expect(page).to have_content('Flat')
     expect(page).to_not have_content('Cottage 1')
   end
+
+  scenario 'letter can specify when their property is available' do
+    letter = User.register(name: "Name", username: "username", email: "test@test.com", password: "password")
+    property = Property.create(name: 'Cottage 1', description: 'A lovely place', ppn: 15, letter_id: letter.id)
+
+    visit('/')
+    click_button 'login'
+    fill_in :username, with: 'username'
+    fill_in :password, with: 'password'
+    click_button('submit')
+    click_button('account_page')
+    first('.property').click_button 'update'
+
+    fill_in :day, with: 06
+    # select :name => 'jan', :from => 'months'
+
+
+    page.select 'January', from: 'months'
+    fill_in :year, with: 2019
+
+    click_button('add')
+
+    page.within('.table') do
+      find('.property:nth-child(1)').click_button('View More')
+    end
+
+    expect(page).to have_content('2019-01-06')
+  end
 end
